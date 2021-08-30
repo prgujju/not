fe0f3cc8c53bea44ebc52134474cfd5553331940
prgujju <89723071+prgujju@users.noreply.github.com>
2021-08-30 11:26:18 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -86,12 +86,12 @@ async def mia(text: Optional[str]=None):
 		with open("font.ttf","rb") as font:
 			font.close()
 	except Exception:
-		font = requests.get("https://firebasestorage.googleapis.com/v0/b/faketrumptweets-8c438.appspot.com/o/font.ttf?alt=media&token=9b1a1497-4284-4a3d-8212-91179f4720ea")
-		with open('font.ttf', 'wb') as f:
-			f.write(font.content)
+		font = requests.get("https://firebasestorage.googleapis.com/v0/b/predit-f5df7.appspot.com/o/whatever%20it%20takes.ttf?alt=media&token=9cfbc822-124c-46e7-8f84-c53946a6583b")
+		with open('take.ttf', 'wb') as f:
+			f.write(take.content)
 			f.close()
 
-	font = ImageFont.truetype("font.ttf", 18)
+	font = ImageFont.truetype("take.ttf", 18)
 	lines = textwrap.wrap(text, width=60)
 	if len(lines) > 1:
 		draw.text((15, 62),"Only 60 character Allow.",fill="#604af0",font=font)
