68ef952805adcd920f705d97658be4415d261065
prgujju <89723071+prgujju@users.noreply.github.com>
2021-09-01 21:31:27 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -66,7 +66,7 @@ async def modi(text: Optional[str]=None,url: Optional[str]=None):
 	if len(lines) > 1:
 		draw.text((15, 62),"Only 60 character Allow.",fill="#604af0",font=font)
 	else:
-		draw.text((15, 57),text,fill="#14171a",font=font)
+		draw.text((15, 57),text,url,fill="#14171a",font=font)
 	img.save("hi.png")
 	file_like = open("./hi.png", mode="rb")
 	return StreamingResponse(file_like, media_type="image/png")
